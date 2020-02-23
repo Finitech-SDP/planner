67fd88635e0bc3baf2d7d164d3f4f520740a6234
Bora M. Alper <bora@boramalper.org>
2020-02-23 19:37:27 +0000

    fix stream resolution

--- stream.sh
+++ stream.sh
@@ -3,4 +3,4 @@
 # https://stackoverflow.com/a/16836338/4466589
 device=${1:-/dev/video2}
 
-vlc -v v4l2://$device:width=1920:height=1080 --v4l2-chroma=UYVY --v4l2-fps=5 --sout "#transcode{vcodec=theo,vb=1024,scale=1,acodec=none,fps=25}:standard{access=http,mux=ogg,dst=:8081/stream.ogg}"
+vlc -v v4l2://$device:width=1280:height=1024 --v4l2-chroma=UYVY --v4l2-fps=5 --sout "#transcode{vcodec=theo,vb=1024,scale=1,acodec=none,fps=25}:standard{access=http,mux=ogg,dst=:8081/stream.ogg}"
