a17f7bff23043bef274c103e410bd7f80e485c11
Bora M. Alper <bora@boramalper.org>
2020-02-23 17:03:24 +0000

    implement local video stream

---
+++ stream.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/16836338/4466589
+device=${1:-/dev/video2}
+
+vlc -v v4l2://$device:width=1920:height=1080 --v4l2-chroma=UYVY --v4l2-fps=5 --sout "#transcode{vcodec=theo,vb=1024,scale=1,acodec=none,fps=25}:standard{access=http,mux=ogg,dst=:8081/stream.ogg}"
